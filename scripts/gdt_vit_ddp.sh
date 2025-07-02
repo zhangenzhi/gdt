d97b6bdb5f2ffb6d574c352297c1e932714647d2
zezzz <zezzz@zezs-mac-mini.lan>
2025-07-02 21:44:13 +0900

    test gdt

--- scripts/gdt_vit_ddp.sh
+++ scripts/gdt_vit_ddp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH -A lrn075
 #SBATCH -o gdt_imagenet_vit_ddp.o%J
-#SBATCH -q debug
 #SBATCH -t 02:00:00
 #SBATCH -N 2
 #SBATCH -p batch
@@ -16,10 +15,12 @@ module load PrgEnv-gnu
 module load gcc-native/12.3
 module load rocm/6.2.0
 
-srun -N 2 -n 8 --ntasks-per-node 8 python main.py \
+srun -N 2 -n 16 --ntasks-per-node 8 python main.py \
     --task gdt_imagenet_vit_ddp \
     --data_dir /lustre/orion/nro108/world-shared/enzhi/dataset/imagenet \
-    --batch_size 128 \
+    --batch_size 256 \
     --num_workers 32 \
     --num_epochs 100 \
-    --savefile gdt-vit-n2
+    --savefile gdt-vit-n2-bz256
+
+###SBATCH -q debug
