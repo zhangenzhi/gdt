b1a4ccb098df1480ae5d3b80f419ee4d7e70dff3
zezzz <zezzz@zezs-mac-mini.lan>
2025-08-19 09:08:32 +0900

    test vit-timm

--- train/vit_imagenet_ddp.py
+++ train/vit_imagenet_ddp.py
@@ -352,7 +352,8 @@ def vit_imagenet_train_single(args, config):
         lr=config['training']['learning_rate'], 
         weight_decay=config['training']['weight_decay'],
         betas=tuple(config['training'].get('betas', (0.9, 0.999))),
-        fused=use_fused # 在CUDA上可用时自动启用融合内核
+        fused=use_fused, # 在CUDA上可用时自动启用融合内核
+        amsgrad=True
     )
     
      # *** 修改: 创建包含线性预热和余弦退火的组合调度器 ***
