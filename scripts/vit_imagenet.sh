586fbed76ddeb84ed77cda7718091ed78e18b97d
Enzhi Zhang <zhangenzhi657@gmail.com>
2025-09-16 11:44:49 +0900

    test vit-l imagenet-1k.

--- scripts/vit_imagenet.sh
+++ scripts/vit_imagenet.sh
@@ -2,7 +2,7 @@
 #SBATCH -A lrn075
 #SBATCH -o imagenet_vit-l16.o%J
 #SBATCH -t 02:00:00
-#SBATCH -N 2
+#SBATCH -N 4
 #SBATCH -p batch
 #SBATCH --mail-user=zhangsuiyu657@gmail.com
 #SBATCH --mail-type=END
@@ -31,10 +31,10 @@ echo "Modules loaded."
 #
 # Note: Hyperparameters like epochs and batch size are now controlled by the config file.
 echo "Launching distributed training..."
-srun -N 2 -n 16 --ntasks-per-node 8 python ./main.py \
-    --config ./configs/vit-l-imagenet.yaml \
+srun -N 4 -n 32 --ntasks-per-node 8 python ./main.py \
+    --config ./configs/vit-l-IN1K.yaml \
     --data_dir /lustre/orion/nro108/world-shared/enzhi/dataset/imagenet \
-    --savefile vit-l-n2-bz4k-he-timm-aa \
+    --savefile vit-l-n4-bz4k-he-timm-aa \
     --num_workers 32 \
     --reload
 
