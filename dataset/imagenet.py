7709728833e750e1681b95a48c9f2becb11a435c
Enzhi Zhang <zhangenzhi657@gmail.com>
2025-09-18 17:04:57 +0900

    test vit-l imagenet-1k.

--- dataset/imagenet.py
+++ dataset/imagenet.py
@@ -682,7 +682,7 @@ import time
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='SHF Quadtree Dataloader with Timm Augmentation Test')
     parser.add_argument('--data_dir', type=str, default="/work/c30636/dataset/imagenet/", help='Path to the ImageNet dataset.')
-    parser.add_argument('--batch_size', type=int, default=512, help='Batch size for DataLoader.')
+    parser.add_argument('--batch_size', type=int, default=1024, help='Batch size for DataLoader.')
     parser.add_argument('--num_workers', type=int, default=32, help='Number of workers for DataLoader.')
     parser.add_argument('--visualize', action='store_true', help='Generate and save a visualization of one batch.')
     args = parser.parse_args()
