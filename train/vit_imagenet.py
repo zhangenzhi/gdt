eefb847e73080092a7f79585decf378ea88dcdd4
张恩智 <zhangenzhi@zezzz.local>
2025-10-05 11:49:43 +0900

    test cache in memory IN1K datalaoder.

--- train/vit_imagenet.py
+++ train/vit_imagenet.py
@@ -20,7 +20,8 @@ from torch.amp import GradScaler, autocast
 sys.path.append("./")
 from dataset.imagenet import imagenet_distribute, imagenet_subloaders
 # from model.vit import create_vit_model
-from model.vit import create_timm_vit as create_vit_model
+# from model.vit import create_timm_vit as create_vit_model
+from model.vit import create_vit_model
 
 from dataset.utlis import param_groups_lrd
 
