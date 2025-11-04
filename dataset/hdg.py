609b79e9690b4aaf1c97633c50aedb86818f0345
Enzhi Zhang <zhangenzhi657@gmail.com>
2025-11-04 20:16:41 +0900

    test unetr s8d.

--- dataset/hdg.py
+++ dataset/hdg.py
@@ -91,19 +91,19 @@ def get_transforms(img_size, mean, std, is_train=True):
     if is_train:
         return A.Compose([
             A.Resize(img_size, img_size),
-            A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.5),
-            A.RandomRotate90(p=0.5),
-            A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45, p=0.5),
-            A.OneOf([
-                A.ElasticTransform(p=0.3),
-                A.GridDistortion(p=0.3),
-                A.OpticalDistortion(p=0.3)
-            ], p=0.3),
-            A.OneOf([
-                A.RandomBrightnessContrast(p=0.5),
-                A.RandomGamma(p=0.5),
-            ], p=0.5),
+            # A.HorizontalFlip(p=0.5),
+            # A.VerticalFlip(p=0.5),
+            # A.RandomRotate90(p=0.5),
+            # A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=45, p=0.5),
+            # A.OneOf([
+            #     A.ElasticTransform(p=0.3),
+            #     A.GridDistortion(p=0.3),
+            #     A.OpticalDistortion(p=0.3)
+            # ], p=0.3),
+            # A.OneOf([
+            #     A.RandomBrightnessContrast(p=0.5),
+            #     A.RandomGamma(p=0.5),
+            # ], p=0.5),
             A.Normalize(mean=(mean,), std=(std,)),
             ToTensorV2(),
         ])
