63002b0f9d8b35cf5c3bebc25628426b9e01e6a0
zhangenzhi <zhangenzhi657@gmail.com>
2026-01-25 20:12:50 +0900

    test dataloader.

--- dataset/s8d_hmae.py
+++ dataset/s8d_hmae.py
@@ -102,7 +102,7 @@ class HDEPretrainDataset(Dataset):
 def get_hde_dataloader(data_root, batch_size=4, num_workers=4):
     dataset = HDEPretrainDataset(
         root_dir=data_root,
-        fixed_length=8194,
+        fixed_length=4096,
         common_patch_size=32
     )
     
@@ -135,7 +135,7 @@ if __name__ == "__main__":
     print(f"Batch Loaded. Visualizing first sample and calculating stats...")
     
     # --- Visualization Parameters ---
-    VIS_SIZE = 8192 # Size of the visualization canvas
+    VIS_SIZE = 2048 # Size of the visualization canvas
     sample_idx = 0
     
     # 1. Load Original Image & Generate Edges
@@ -151,7 +151,7 @@ if __name__ == "__main__":
     else:
         # 复现 Dataset 中的预处理步骤 (在全分辨率上计算 Canny)
         blurred_full = cv2.GaussianBlur(full_img, (5, 5), 0)
-        edges_full = cv2.Canny(blurred_full, 50, 130)
+        edges_full = cv2.Canny(blurred_full, 50, 100)
         
         # 【新增】: 膨胀边缘，防止在 resize 时消失
         # 8K图片线条太细，resize到2K时如果不加粗，插值后会变成几乎看不见的灰色
